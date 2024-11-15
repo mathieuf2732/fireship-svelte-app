0e9ff564140e6b14e55667dd17cdc4f341b374c7
Mathieu <mathieu.ford@gmail.com>
2024-11-14 20:13:59 -0500

    Setup firebase client sdk

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyA994langTIyKXUZ0LG6YMb7K8j9dSTL6E',
+	authDomain: 'svelte-melvin.firebaseapp.com',
+	projectId: 'svelte-melvin',
+	storageBucket: 'svelte-melvin.firebasestorage.app',
+	messagingSenderId: '76784658428',
+	appId: '1:76784658428:web:65279cb8a4aad232893479'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
