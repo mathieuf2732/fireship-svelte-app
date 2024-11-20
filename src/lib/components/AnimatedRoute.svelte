5faed040f9b5bc7139ad4bb7e5298e8ee05e6ba3
Mathieu <mathieu.ford@gmail.com>
2024-11-19 20:44:54 -0500

    Implemented AnimatedRoutes

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import type { Snippet } from "svelte";
+	import { fly } from "svelte/transition";
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+{#key $page.url}
+	<div in:fly={{ x: "-100%", duration: 500 }}>
+		{@render children?.()}
+	</div>
+{/key}
