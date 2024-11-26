1aa67b547b33dbb2fb639ed2563f08cfc3c13479
McLovin3 <mathieu.ford@gmail.com>
2024-11-25 23:17:59 -0500

    Typed states

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -5,9 +5,9 @@
 
 	const USERNAME_VALIDATION_REGEX = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
 
-	let username = $state("");
-	let loading = $state(false);
-	let isAvailable = $state(false);
+	let username = $state<string>("");
+	let loading = $state<boolean>(false);
+	let isAvailable = $state<boolean>(false);
 	let debounceTimer: NodeJS.Timeout;
 
 	let isValid = $derived(
