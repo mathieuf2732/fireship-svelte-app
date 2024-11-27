bbb38f13cc571c163077bbece9e76b1ec8ef7e85
Mathieu <mathieu.ford@gmail.com>
2024-11-27 18:43:09 -0500

    Added the [username] route

---
+++ src/routes/[username]/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
+</script>
+
+<svelte:head>
+	<title>@{data.username}</title>
+	<meta name="Description" content={data.bio} />
+</svelte:head>
+
+<main class="prose text-center mx-auto mt-8">
+	<h1 class="text-7xl text-purple-500">
+		@{data.username}
+	</h1>
+
+	<img src={data.photoURL ?? "/user.png"} alt="photoUrl" width="256" class="mx-auto" />
+
+	<p class="text-xl my-8">{data.bio ?? "No bio"}</p>
+
+	<ul class="list-none">
+		{#each data.links as item}
+			{@debug item}
+		{/each}
+	</ul>
+</main>
