248d00f9216b54328c1608210327053dee802617
Mathieu <mathieu.ford@gmail.com>
2024-12-04 21:48:37 -0500

    .env fixes

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -1,11 +1,15 @@
 import { getAuth } from "firebase-admin/auth";
-import { FIREBASE_SERVICE_ACCOUNT } from "$env/static/private";
+import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from "$env/static/private";
 import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 
 try {
 	admin.initializeApp({
-		credential: admin.credential.cert(FIREBASE_SERVICE_ACCOUNT)
+		credential: admin.credential.cert({
+			clientEmail: FB_CLIENT_EMAIL,
+			privateKey: FB_PRIVATE_KEY,
+			projectId: FB_PROJECT_ID
+		})
 	});
 } catch (e: unknown) {
 	if (e instanceof Error && !/already exists/.test(e.message)) {
